28498bfa576e9c94abd8bbfa1dc7d111bfa2195e
Azie88 <obandoandrew8@gmail.com>
2023-12-09 16:29:51 +0300

    Sepsis API

--- main.py
+++ main.py
@@ -1,78 +1,85 @@
-from fastapi import FastAPI
-import joblib
-from  pydantic import BaseModel
+from fastapi import FastAPI,HTTPException,Response,status
+import joblib 
+from pydantic import BaseModel
 import pandas as pd
 import numpy as np
-import uvicorn
+import os ,uvicorn
 import logging
 
-#load pipeline
+
+# Additional information to include in app description
+Util_info = """
+- PRG: Plasma Glucose
+- PL: Blood Work Result-1 (mu U/ml)
+- PR: Blood Pressure (mm Hg)
+- SK: Blood Work Result-2 (mm)
+- TS: Blood Work Result-3 (mu U/ml)
+- M11: Body Mass Index (weight in kg/(height in m)^2)
+- BD2: Blood Work Result-4 (mu U/ml)
+- Age: Patient's Age (years)
+- Insurance: If a patient holds a valid insurance card
+
+Output:
+- Sepsis: Positive if a patient in ICU will develop sepsis, Negative if a patient in ICU will not develop sepsis.
+"""
+
+#load pipeline and encoder
 pipeline  = joblib.load('toolkit/pipeline.joblib')
 encoder  = joblib.load('toolkit/encoder.joblib')
 
-# Configure logs
-import logging
+# Configure the logs
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-#Config
+# APP
 app = FastAPI(
-    title = 'Sepsis Prediction App',
-    version = '0.0.1',
-    description = 'This API will predict if a patient has Sepsis or not'
+    title='Sepsis Prediction App',
+    description= Util_info
 )
 
-# Create class to define our input type
-class Sepsis_features(BaseModel):
-    PRG:int
-    PL:int
-    PR:int
-    SK:int
-    TS:int
+#Create class to define our input
+class Sepsis_Feature(BaseModel):
+    PRG:float
+    PL:float
+    PR:float
+    SK:float
+    TS:float
     M11:float
     BD2:float
-    Age:int
-    Insurance:float
+    Age:float
+    Insurance : float
 
 
-@app.get('/')
+#ENDPOINTS
+
+@app.get("/")
 async def home():
-    return {'Hello world'}
+    return 'Welcome to the Sepsis Prediction App'
 
-
-@app.post('/predict')
-async def Predict_Sepsis(input:Sepsis_features):
-    "function that receive the posted input data,do the operation and return an output /error message"
-    output ={}   
+@app.post('/predict',status_code=status.HTTP_201_CREATED)
+async def Predict_Sepsis(input:Sepsis_Feature):
  
     try:
-        data = {[Sepsis_features.model_dump]}
-
-        df = pd.DataFrame([data])
+        df = pd.DataFrame([input.model_dump()])
         prediction_probs = pipeline.predict_proba(df)
         prediction_class = pipeline.predict(df)
+        decoder_prediction = encoder.inverse_transform([prediction_class])
+        
+        print(f"[iNFO] Input data as dataframe:\n{decoder_prediction.tolist()}")
 
-        if(prediction_class[0].tolist() ==0):
-            response = "Negative"
-        if(prediction_class[0].tolist() ==1 ):
-            response ="Positive"
-
-        output = {
-            "data": data,
-            "predicted_class": prediction_class[0].tolist(),
+        return {
+            "data" : input,
+            "predicted_class" : prediction_class[0].tolist(),
             "prediction": prediction_probs[0].tolist(),
-            "result":response
+            "result":decoder_prediction.tolist() 
         }
-
+    
     except ValueError as e :
         logger.error(f"ValueError: {e}")
-        output ={ "error":str(e)}
+        return { "error":str(e)}
         
     except Exception as e :
-        output ={"error ": f"OOps something went wrong :\n{e}"}
-    
-    finally:
-        return output # output must be json serializable
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=f'this is a server error contact administrator {str(e)}')     
 
 if __name__ == "__main__":
     uvicorn.run("main:app",reload=True)
